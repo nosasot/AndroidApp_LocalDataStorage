2617fb3bc266f53ca233c6f002fb7e76254d2069
ANN <tnmn47@gmail.com>
2018-09-16 11:25:47 +0700

    add executeTransactionAsync

--- app/src/main/java/com/arit/demo/localstorage/BaseApplication.java
+++ app/src/main/java/com/arit/demo/localstorage/BaseApplication.java
@@ -6,9 +6,12 @@ import android.content.SharedPreferences;
 import com.arit.demo.localstorage.model.User;
 
 import io.realm.Realm;
+import io.realm.RealmAsyncTask;
 import io.realm.RealmConfiguration;
 
 public class BaseApplication extends Application {
+
+    private RealmAsyncTask task;
 
     @Override
     public void onCreate() {
@@ -21,32 +24,55 @@ public class BaseApplication extends Application {
         editor.apply();
 
         Realm.init(this);
-//        RealmConfiguration config = RealmConfiguration
-//                                    .Builder(getApplicationContext())
-//                                    .name("localstorgename.realm")
-//                                    .build();
 
-//        RealmConfiguration config = new RealmConfiguration()
-//                                        .Builder()
-//                                        .name("Local.realm").deleteAll
+        RealmConfiguration config  = new RealmConfiguration.Builder()
+                                        .name("Local.realm").deleteRealmIfMigrationNeeded()
+                                        .build();
+        Realm.setDefaultConfiguration(config);
 
         Realm realm = Realm.getDefaultInstance();
 
-//      User user = realm.createObject(User.class); // Managed Object
+        //User user = realm.createObject(User.class); // Managed Object
         final User user = new User(); // UnManaged Object
         user.setFirstname("John");
         user.setLastname("Doe");
 
-        realm.executeTransaction(new Realm.Transaction() {
+        /*realm.executeTransaction(new Realm.Transaction() {
             @Override
             public void execute(Realm realm) {
                 realm.insert(user);
             }
+        });*/
+
+        // 1) กำลัง process อยู่แต่ว่ามีสายเข้า ทำให้ process ค้าง เลยต้องทำการเคลียร์ของ async ทิ้ง มี call back 2ตัว onsuccess,onerror ทำอะไรต่อ
+        task = realm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                realm.insert(user);
+            }
+        }, new Realm.Transaction.OnSuccess() {
+            @Override
+            public void onSuccess() {
+                // do something when finished task
+            }
+        }, new Realm.Transaction.OnError() {
+            @Override
+            public void onError(Throwable error) {
+                // do something when finished task
+            }
         });
+
     }
 
     @Override
     public void onTerminate() {
         super.onTerminate();
+
+        // 2) Kill task
+        Realm.getDefaultInstance().close();
+        this.task.cancel();
     }
+
+
+
 }
